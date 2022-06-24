a78c7a290f6bf8514bfb7da92a5053f8639a7a26
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2022-06-24 04:32:09 +0200

    Test

--- __init__.py
+++ __init__.py
@@ -15,8 +15,12 @@ import bpy
 import struct
 
 from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Operator
+from bpy.props import StringProperty, BoolProperty, EnumProperty, PointerProperty
+from bpy.types import (Panel,
+                       Operator,
+                       AddonPreferences,
+                       PropertyGroup,
+                       )
 
 
 class ImportRidgeRacer(Operator, ImportHelper):
@@ -37,6 +41,31 @@ class ImportRidgeRacer(Operator, ImportHelper):
         from . import  import_ridgeRacer
         return import_ridgeRacer.main(self.filepath, self.clear_scene)
 
+class MyProperties(PropertyGroup):
+
+    path : StringProperty(
+        name="",
+        description="Path to Directory",
+        default="",
+        maxlen=1024,
+        subtype='DIR_PATH')
+
+class OBJECT_PT_CustomPanel(Panel):
+    bl_idname = "OBJECT_PT_my_panel"
+    bl_label = "My Panel"
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "UI"
+    bl_category = "New Tab"
+
+
+    def draw(self, context):
+        layout = self.layout
+        scn = context.scene
+        col = layout.column(align=True)
+        col.prop(scn.my_tool, "path", text="")
+
+
+classes = (MyProperties, ImportRidgeRacer, OBJECT_PT_CustomPanel)
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_import(self, context):
@@ -44,13 +73,21 @@ def menu_func_import(self, context):
 
 
 def register():
-    bpy.utils.register_class(ImportRidgeRacer)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+
+    bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
 
 
 def unregister():
-    bpy.utils.unregister_class(ImportRidgeRacer)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
+    del bpy.types.Scene.my_tool
 
 
 if __name__ == "__main__":
