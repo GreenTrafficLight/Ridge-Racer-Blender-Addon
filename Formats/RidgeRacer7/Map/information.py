1a9e69d78ac276d94ab25a15053d54fbd86f1f66
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2025-01-30 10:00:09 +0100

    Add object transformation reading

--- Formats/RidgeRacer7/Map/information.py
+++ Formats/RidgeRacer7/Map/information.py
@@ -1,29 +1,62 @@
 from ....Utilities.binaryReader import *
 
-class OBJECT_INFORMATION_CONTAINER:
+from typing import List
+
+class ObjectInformationContainer:
     def __init__(self):
         self.offset = 0
         self.count = 0
 
-class OBJECT_INFORMATION:
+    def read(self, br: BinaryReader):
+        self.offset = br.readUInt()
+        self.count = br.readUInt()
+
+class ObjectTransformation:
+
+    def __init__(self):
+        self.unknown_vector4_1 = None
+        self.unknown_vector4_2 = None
+        self.transformation_matrix = None
+
+    def read(self, br: BinaryReader):
+        self.unknown_vector4_1 = br.readVector4f()
+        self.unknown_vector4_2 = br.readVector4f()
+        self.transformation_matrix = br.readMatrix4x4f()
+
+class ObjectInformation:
 
     def __init__(self):
         self.name = ""
         self.type = ""
-        self.containers = []
+        self.containers: List[ObjectInformationContainer] = []
+        self.transformations: List[ObjectInformation] = []
 
     def read(self, br: BinaryReader):
         self.name = br.bytesToString(br.readBytes(32)).replace("\0", "")
         self.type = br.bytesToString(br.readBytes(32)).replace("\0", "")
 
         for i in range(4):
-            # map_information_data 1 = objet transformations
-            # map_information_data 2 = ?
-            # map_information_data 3 = ?
-            # map_information_data 4 = ?
+            # 1 = offset that contains objet transformations
+            # 2 = ?
+            # 3 = ?
+            # 4 = ?
             
-            map_information_container = OBJECT_INFORMATION_CONTAINER()
-            map_information_container.offset = br.readUInt()
-            map_information_container.count = br.readUInt()
+            map_information_container = ObjectInformationContainer()
+            map_information_container.read(br)
 
             self.containers.append(map_information_container)
+
+        br.seek(self.containers[0].offset, 0)
+        self.readObjetTransformations(br)
+
+    def readObjetTransformations(self, br: BinaryReader):
+        unkOffset1 = br.readUInt()
+        br.seek(unkOffset1, 0)
+        br.readUInt()
+        objectTransformationsOffset = br.readUInt()
+        objectTransformationsCount = br.readUInt()
+        br.seek(objectTransformationsOffset, 0)
+        for objectTransformation in objectTransformationsCount:
+            transformation = ObjectTransformation()
+            transformation.read(br)
+            self.transformations.append(transformation)
--- Formats/RidgeRacer7/Map/map.py
+++ Formats/RidgeRacer7/Map/map.py
@@ -1,17 +1,19 @@
 from ....Utilities.binaryReader import *
 
+from .information import *
+
 class MAP:
 
     def __init__(self):
         pass
 
     def read(self, br: BinaryReader):
-        object_informations_offset = br.readUInt()
+        offset1 = br.readUInt()
         br.readUInt()
 
         file_size = br.readUInt()
 
-        br.seek(object_informations_offset, 0)
+        br.seek(offset1, 0)
         
         object_information_offsets = []
         object_informations_count = br.readUInt()
@@ -21,5 +23,10 @@ class MAP:
 
         for i in range(object_informations_count):
             object_information_offsets.append(br.readUInt())
+
+        for offset in object_information_offsets:
+            br.seek(offset, 0)
+            information = ObjectInformation()
+            information.read(br)
 
         
